4c639ebe38453159dfa9ff145d1288100bd0a762
leandro-jr <leandro.jr@outlook.com>
2022-06-08 14:37:17 -0300

    praticando commit inicial

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def insert_estoque():
+    estoque_dict = {}
+    print("Entre com os produtos no estoque. Quando estiver satisfeito escreva 'pare'")
+    while True:
+        produto = input("Insira produto: ").strip().lower()
+        if produto == "pare":
+            break
+        quantidade = input("Insira quantidade: ")
+        if quantidade == "pare":
+            break
+        estoque_dict[produto] = quantidade
+
+    print()
+    for produto, quant in estoque_dict.items():
+        print(f"{produto.title()}: {quantidade}")
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    insert_estoque()
